d36e3aecaca346e32fdba536510447362a82eea7
bibenga <{ID}+{username}@users.noreply.github.com>
2023-08-14 19:40:33 +0000

    adds sqlite

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <sqlite3.h>
 
 int main()
 {
     std::string a = "Olala";
     std::cout << "Hello World -> " << a << std::endl;
+
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open(".palabras.db", &db);
+
+    if (rc)
+    {
+        std::cout << "Can't open database:" << sqlite3_errmsg(db) << std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cout << "Opened database successfully" << std::endl;
+    }
+
+    sqlite3_close(db);
 }
